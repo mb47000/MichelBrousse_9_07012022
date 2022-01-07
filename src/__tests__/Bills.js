5ab2e4c038b889d1eb4fd7a440282f47e2487f47
Brousse <mbweb47@gmail.com>
2022-01-07 23:18:19 +0100

    ✅ Test - Add for views/Bills 100% cover

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -1,14 +1,56 @@
+/**
+ * @jest-environment jsdom
+ */
+
 import { screen } from "@testing-library/dom"
 import BillsUI from "../views/BillsUI.js"
 import { bills } from "../fixtures/bills.js"
+import {
+  getByTestId, getAllByText
+} from '@testing-library/dom'
+import { localStorageMock } from "../__mocks__/localStorage.js";
+import Router from "../app/Router.js";
+import { ROUTES_PATH } from "../constants/routes";
 
 describe("Given I am connected as an employee", () => {
-  describe("When I am on Bills Page", () => {
-    test("Then bill icon in vertical layout should be highlighted", () => {
-      const html = BillsUI({ data: []})
-      document.body.innerHTML = html
-      //to-do write expect expression
+  Object.defineProperty(window, "localStorage", {
+    value: localStorageMock,
+  });
+  window.localStorage.setItem(
+    "user",
+    JSON.stringify({
+      type: "Employee",
     })
+  );
+
+  describe("When I am on Bills Page", () => {
+    Object.defineProperty(window, "location", {
+      value: { hash: ROUTES_PATH["Bills"] },
+    });
+    document.body.innerHTML = `<div id="root"></div>`;
+    
+    test("Then bill icon in vertical layout should be highlighted", () => {
+      Router();
+      const billIcon = getByTestId(document.body, "icon-window")
+      expect(billIcon).toBeTruthy();
+      expect(billIcon.classList.contains("active-icon")).toBeTruthy();
+    })
+
+    describe("But it is loading ", () => {
+      test("Then I should land on a loading page", () => {
+        Router();
+        expect(getAllByText(document.body, "Loading...")).toBeTruthy();
+      })
+    })
+    
+    describe("But an error message has been thrown", () => {
+      test("Then Error page should be rendrered", () => {
+        const html = BillsUI({ error: "some error message" });
+        document.body.innerHTML = html;
+        expect(getAllByText(document.body,"some error message")).toBeTruthy();
+      })
+    })
+
     test("Then bills should be ordered from earliest to latest", () => {
       const html = BillsUI({ data: bills })
       document.body.innerHTML = html
